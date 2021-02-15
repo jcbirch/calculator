1176ce826a4582c491f5dab41357b217e779e460
jcbirch <jessebirch@icloud.com>
2021-02-15 14:07:39 -0600

    finalized css and html for styling

---
+++ main.js
@@ -0,0 +1,25 @@
+function addNumbers(a, b) {
+     return a + b;
+}
+
+function subtractNumbers(a, b) {
+    return a - b;
+}
+
+function multiplyNumbers(a, b) {
+    return a * b;
+}
+
+function divideNumbers(a, b) {
+    return a / b;
+}
+
+
+function operate(a, b, operator) {
+    a = 4;
+    b = 5;
+    return operator = addNumbers(a, b);
+    
+}
+
+console.log(operate());
